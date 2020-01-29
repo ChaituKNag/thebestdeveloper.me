c222c1d6ac1bbe4083554887c78bea56615932a0
nkonada <knc@outlook.in>
2020-01-29 23:12:39 +0530

    Added some intro text.

--- site/src/components/styled/BannerText.js
+++ site/src/components/styled/BannerText.js
@@ -1,9 +1,26 @@
 import React from "react"
 import { Typography } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
 
-const BannerText = ({ children }) => {
+const useStyles = makeStyles(theme => ({
+  text: {
+    fontFamily: "Acme",
+    padding: "50px 0",
+  },
+}))
+
+const BannerText = ({ children, align, ...restProps }) => {
+  const classes = useStyles()
+
   return (
-    <Typography align="center" variant="h1" component="h2" gutterBottom>
+    <Typography
+      align={align || "left"}
+      variant="h1"
+      component="h2"
+      gutterBottom
+      className={classes.text}
+      {...restProps}
+    >
       {children}
     </Typography>
   )
