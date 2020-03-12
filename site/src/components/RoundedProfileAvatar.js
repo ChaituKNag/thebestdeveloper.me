f90feff7e02fb08ec069a4eec872f3df7616fca5
nkonada <knc@outlook.in>
2020-03-12 21:59:38 +0530

    correct query updated

--- site/src/components/RoundedProfileAvatar.js
+++ site/src/components/RoundedProfileAvatar.js
@@ -7,25 +7,34 @@ const ContainerDiv = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 5vw;
+  margin-top: 50px;
+
+  @media (max-width: 960px) {
+    margin-top: 20px;
+  }
 `
 
 const RoundedImg = styled.div`
   border-radius: 50%;
-  max-width: 300px;
+  max-width: 400px;
   box-shadow: 10px 10px 15px -5px lightgrey;
   border: 10px solid white;
   overflow: hidden;
   flex: 1;
+
+  @media (max-width: 960px) {
+    max-width: 300px;
+  }
 `
 
 const RoundedProfileAvatar = () => {
   const data = useStaticQuery(graphql`
-    query ProfileQuery {
-      imageSharp(id: { eq: "c9f392eb-3356-501e-ab90-6e5173d12ef2" }) {
-        id
-        fluid {
-          ...GatsbyImageSharpFluid
+    query ProfileAvatarQuery {
+      file(relativePath: { eq: "smiley-with-coat.jpeg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
         }
       }
     }
@@ -34,7 +43,7 @@ const RoundedProfileAvatar = () => {
     <ContainerDiv>
       <RoundedImg>
         <Img
-          fluid={data.imageSharp.fluid}
+          fluid={data.file.childImageSharp.fluid}
           alt="Naga Chaitanya Konada's profile pic"
         />
       </RoundedImg>
