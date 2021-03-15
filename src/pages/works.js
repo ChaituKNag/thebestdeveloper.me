64c85fd8894ca94cc99f9f8db74467da78e7a226
Naga Chaitanya Konada <knc.dev@outlook.com>
2021-03-15 08:55:59 +0530

    updated works page

--- src/pages/works.js
+++ src/pages/works.js
@@ -6,6 +6,8 @@ import styled from "styled-components"
 import marked from "marked"
 import Img from "gatsby-image"
 import Text from "../components/styled/Text"
+import { Column } from "../components/styled/Container"
+import { OutlineButton, SolidButton } from "../components/styled/Button"
 
 const ImageBox = styled.div`
   width: 100%;
@@ -44,31 +46,38 @@ const Works = () => {
 
   return (
     <Layout>
-      <PageTitle>{content.title}</PageTitle>
-      <Text>{content.disclaimer}</Text>
-      {content.items.map((item, i) => (
-        <div key={`work-item-${i}`}>
-          <ItemTitle>{item.workTitle}</ItemTitle>
-          <Text
-            as="div"
-            dangerouslySetInnerHTML={{ __html: marked(item.workDescription) }}
-          ></Text>
-          {item.workPreview && item.workPreview.childImageSharp ? (
-            <>
-              <Text as="h4">Preview: </Text>
-              <ImageBox>
-                <Img fluid={item.workPreview.childImageSharp.fluid} />
-              </ImageBox>
-            </>
-          ) : null}
-          <Link href={item.workDemoLink} target="_blank">
-            Demo
-          </Link>
-          <Link href={item.workSourceCode} target="_blank">
-            Source Code
-          </Link>
-        </div>
-      ))}
+      <Column>
+        <PageTitle>{content.title}</PageTitle>
+        <Text>{content.disclaimer}</Text>
+        {content.items.map((item, i) => (
+          <div key={`work-item-${i}`}>
+            <ItemTitle>{item.workTitle}</ItemTitle>
+            <Text
+              as="div"
+              dangerouslySetInnerHTML={{ __html: marked(item.workDescription) }}
+            ></Text>
+            {item.workPreview && item.workPreview.childImageSharp ? (
+              <>
+                <Text as="h4">Preview: </Text>
+                <ImageBox>
+                  <Img fluid={item.workPreview.childImageSharp.fluid} />
+                </ImageBox>
+              </>
+            ) : null}
+            <OutlineButton
+              margin="0 .5rem 0 0"
+              as="a"
+              href={item.workDemoLink}
+              target="_blank"
+            >
+              Demo
+            </OutlineButton>
+            <SolidButton as="a" href={item.workSourceCode} target="_blank">
+              Source Code
+            </SolidButton>
+          </div>
+        ))}
+      </Column>
     </Layout>
   )
 }
